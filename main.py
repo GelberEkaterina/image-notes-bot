e6babfa9f8010fe13c1ba4f328f31e15df2cedce
GelberEkaterina <ek.gelber@gmail.com>
2024-04-22 00:44:06 +0700

    Base

---
+++ main.py
@@ -0,0 +1,116 @@
+import logging
+
+import aiohttp
+from telegram.ext import Application, MessageHandler, filters, CommandHandler, ConversationHandler
+from aiogram import Bot, Dispatcher, types
+from aiogram.types import ReplyKeyboardRemove, ReplyKeyboardMarkup, KeyboardButton
+
+BOT_TOKEN = '7116319751:AAGoO4z5EKLLt8cML5seEGlhBFNF7_5K6ow'
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.DEBUG
+)
+
+logger = logging.getLogger(__name__)
+bot = Bot(token=BOT_TOKEN)
+dp = Dispatcher(bot)
+
+
+async def help_command(update, context):
+    """Отправляет сообщение когда получена команда /help"""
+    await update.message.reply_text("Я умею вести диалог из двух вопросов.")
+
+
+async def start(update, context):
+    await update.message.reply_text(
+        "Привет. Пройдите небольшой опрос, пожалуйста!\n"
+        "Вы можете прервать опрос, послав команду /stop.\n"
+        "В каком городе вы живёте?")
+
+    # Число-ключ в словаре states —
+    # втором параметре ConversationHandler'а.
+    return 1
+    # Оно указывает, что дальше на сообщения от этого пользователя
+    # должен отвечать обработчик states[1].
+    # До этого момента обработчиков текстовых сообщений
+    # для этого пользователя не существовало,
+    # поэтому текстовые сообщения игнорировались.
+
+
+# Добавили словарь user_data в параметры.
+async def first_response(update, context):
+    # Это ответ на первый вопрос.
+    # Мы можем использовать его во втором вопросе.
+    # Сохраняем ответ в словаре.
+    context.user_data['locality'] = update.message.text
+    locality = update.message.text
+    await update.message.reply_text(
+        f"Какая погода в городе {locality}?")
+    # Следующее текстовое сообщение будет обработано
+    # обработчиком states[2]
+    return 2
+
+
+async def second_response(update, context):
+    weather = update.message.text
+    logger.info(weather)
+    # Используем user_data в ответе.
+    await update.message.reply_text(
+        f"Спасибо за участие в опросе! Привет, {context.user_data['locality']}!")
+    context.user_data.clear()  # очищаем словарь с пользовательскими данными
+    return ConversationHandler.END  # Константа, означающая конец диалога.
+
+
+@dp.message_handler(commands=['start'])
+async def send_welcome(message: types.Message):
+   kb = [
+       [
+           types.KeyboardButton(text="Сможешь повторить это?"),
+           types.KeyboardButton(text="А это?")
+       ],
+   ]
+   keyboard = types.ReplyKeyboardMarkup(keyboard=kb)
+
+
+async def stop(update, context):
+    await update.message.reply_text("Всего доброго!")
+    return ConversationHandler.END
+
+
+async def get_response(url, params):
+    logger.info(f"getting {url}")
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url, params=params) as resp:
+            return await resp.json()
+
+
+def main():
+    text_handler = MessageHandler(filters.TEXT & ~filters.COMMAND, first_response)
+    application = Application.builder().token(BOT_TOKEN).build()
+    application.add_handler(text_handler)
+    application.add_handler(CommandHandler("help", help_command))
+    conv_handler = ConversationHandler(
+        # Точка входа в диалог.
+        # В данном случае — команда /start. Она задаёт первый вопрос.
+        entry_points=[CommandHandler('start', start)],
+
+        # Состояние внутри диалога.
+        # Вариант с двумя обработчиками, фильтрующими текстовые сообщения.
+        states={
+            # Функция читает ответ на первый вопрос и задаёт второй.
+            1: [MessageHandler(filters.TEXT & ~filters.COMMAND, first_response)],
+            # Функция читает ответ на второй вопрос и завершает диалог.
+            2: [MessageHandler(filters.TEXT & ~filters.COMMAND, second_response)]
+        },
+
+        # Точка прерывания диалога. В данном случае — команда /stop.
+        fallbacks=[CommandHandler('stop', stop)]
+    )
+
+    application.add_handler(conv_handler)
+
+    application.add_handler(geo_dialog_handler())
+    application.run_polling()
+
+
+if __name__ == '__main__':
+    main()
