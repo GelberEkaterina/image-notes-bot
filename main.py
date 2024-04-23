afc07211b48dd393d09ced0ac45f563430285b37
GelberEkaterina <ek.gelber@gmail.com>
2024-04-24 04:23:22 +0700

    Base

--- main.py
+++ main.py
@@ -1,115 +1,63 @@
 import logging
-
-import aiohttp
-from telegram.ext import Application, MessageHandler, filters, CommandHandler, ConversationHandler
+import asyncio
 from aiogram import Bot, Dispatcher, types
-from aiogram.client.default import DefaultBotProperties
-from aiogram.types import ReplyKeyboardRemove, ReplyKeyboardMarkup, KeyboardButton
+from aiogram.filters.command import Command
+from aiogram import F
+from aiogram.types import Message
 
 BOT_TOKEN = '7116319751:AAGoO4z5EKLLt8cML5seEGlhBFNF7_5K6ow'
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.DEBUG
 )
-
-logger = logging.getLogger(__name__)
-bot = Bot(token=BOT_TOKEN, default=DefaultBotProperties())
+bot = Bot(BOT_TOKEN)
 dp = Dispatcher()
+logger = logging.getLogger(__name__)
 
 
 async def help_command(update, context):
-    """Отправляет сообщение когда получена команда /help"""
-    await update.message.reply_text("Я умею вести диалог из двух вопросов.")
+    await update.message.reply_text("Этот бот может создавать .")
 
 
-async def start(update, context):
-    await update.message.reply_text(
-        "Привет. Пройдите небольшой опрос, пожалуйста!\n"
-        "Вы можете прервать опрос, послав команду /stop.\n"
-        "В каком городе вы живёте?")
-
-    # Число-ключ в словаре states —
-    # втором параметре ConversationHandler'а.
-    return 1
-    # Оно указывает, что дальше на сообщения от этого пользователя
-    # должен отвечать обработчик states[1].
-    # До этого момента обработчиков текстовых сообщений
-    # для этого пользователя не существовало,
-    # поэтому текстовые сообщения игнорировались.
+@dp.message(Command("start"))
+async def start(message: types.Message):
+    kb = [
+        [types.KeyboardButton(text="Создать заметку")],
+        [types.KeyboardButton(text="Посмотреть заметки")],
+        [types.KeyboardButton(text="Посмотреть место")],
+        [types.KeyboardButton(text="Создать напоминание")]
+    ]
+    keyboard = types.ReplyKeyboardMarkup(keyboard=kb)
+    await message.answer("Что Вы хотите сделать?", reply_markup=keyboard)
 
 
-# Добавили словарь user_data в параметры.
-async def first_response(update, context):
-    # Это ответ на первый вопрос.
-    # Мы можем использовать его во втором вопросе.
-    # Сохраняем ответ в словаре.
-    context.user_data['locality'] = update.message.text
-    locality = update.message.text
-    await update.message.reply_text(
-        f"Какая погода в городе {locality}?")
-    # Следующее текстовое сообщение будет обработано
-    # обработчиком states[2]
-    return 2
+@dp.message(F.text.lower() == "создать заметку")
+async def with_puree(message: types.Message):
+    await message.reply("Отправьте изображение-заметку")
 
 
-async def second_response(update, context):
-    weather = update.message.text
-    logger.info(weather)
-    # Используем user_data в ответе.
-    await update.message.reply_text(
-        f"Спасибо за участие в опросе! Привет, {context.user_data['locality']}!")
-    context.user_data.clear()  # очищаем словарь с пользовательскими данными
-    return ConversationHandler.END  # Константа, означающая конец диалога.
+@dp.message(F.text.lower() == "посмотреть заметки")
+async def without_puree(message: types.Message):
+    await message.reply("Какую заметку вы бы хотели посмотреть?")
 
 
-@dp.message(commands=['start'])
-async def send_welcome(message: types.Message):
-   kb = [
-       [
-           types.KeyboardButton(text="Сможешь повторить это?"),
-           types.KeyboardButton(text="А это?")
-       ],
-   ]
-   keyboard = types.ReplyKeyboardMarkup(keyboard=kb)
+@dp.message(F.text.lower() == "посмотреть место")
+async def place(message: types.Message):
+    await message.reply("Введите название места")
 
 
-async def stop(update, context):
-    await update.message.reply_text("Всего доброго!")
-    return ConversationHandler.END
+@dp.message(F.text.lower() == "создать напоминание")
+async def reminder(message: types.Message):
+    await message.reply("Введите текст напоминания")
 
 
-async def get_response(url, params):
-    logger.info(f"getting {url}")
-    async with aiohttp.ClientSession() as session:
-        async with session.get(url, params=params) as resp:
-            return await resp.json()
+@dp.message(F.photo)
+async def echo_gif(message: Message):
+    await message.reply_photo(message.photo[-1].file_id)
 
 
-def main():
-    text_handler = MessageHandler(filters.TEXT & ~filters.COMMAND, first_response)
-    application = Application.builder().token(BOT_TOKEN).build()
-    application.add_handler(text_handler)
-    application.add_handler(CommandHandler("help", help_command))
-    conv_handler = ConversationHandler(
-        # Точка входа в диалог.
-        # В данном случае — команда /start. Она задаёт первый вопрос.
-        entry_points=[CommandHandler('start', start)],
-
-        # Состояние внутри диалога.
-        # Вариант с двумя обработчиками, фильтрующими текстовые сообщения.
-        states={
-            # Функция читает ответ на первый вопрос и задаёт второй.
-            1: [MessageHandler(filters.TEXT & ~filters.COMMAND, first_response)],
-            # Функция читает ответ на второй вопрос и завершает диалог.
-            2: [MessageHandler(filters.TEXT & ~filters.COMMAND, second_response)]
-        },
-
-        # Точка прерывания диалога. В данном случае — команда /stop.
-        fallbacks=[CommandHandler('stop', stop)]
-    )
-
-    application.add_handler(conv_handler)
-    application.run_polling()
+async def main():
+    await dp.start_polling(bot)
 
 
-if __name__ == '__main__':
-    main()
+if __name__ == "__main__":
+    asyncio.run(main())
