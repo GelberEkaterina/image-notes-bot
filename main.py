32a4532493d96654e7da21c5c5bab49c552214e3
GelberEkaterina <ek.gelber@gmail.com>
2024-04-29 01:44:27 +0700

    Removed Some Comments and Text Mistakes

--- main.py
+++ main.py
@@ -18,6 +18,7 @@ dp = Dispatcher()
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.DEBUG
 )
+logging.disable(level=logging.WARNING)
 logger = logging.getLogger(__name__)
 
 Base = declarative_base()
@@ -66,7 +67,7 @@ def get_ll_spn(toponym):
 async def help(update, context):
     await update.message.reply_text(
         "Этот бот может создавать заметки-изображения.\n"
-        "Также, он может отправлять напоминания")
+        "Также, он может отправлять карту места по запросу\n")
     return ConversationHandler.END
 
 
@@ -134,9 +135,11 @@ async def respond(update, context):
 
     if context.user_data['state'] == 'Создать заметку':
         new_note = Photo(photo_id=update.message.photo[-1].file_id, caption=update.message.caption)
+
         if update.message.caption is None:
             await update.message.reply_text('Для создания заметки необходима подпись. Отправьте изображение ещё раз')
             return 2
+
         else:
             session.add(new_note)
             session.commit()
@@ -144,9 +147,11 @@ async def respond(update, context):
 
     elif context.user_data['state'] == 'Открыть заметку':
         note = session.query(Photo).filter_by(caption=update.message.text).order_by(-Photo.id).first()
+
         if note is None:
             await update.message.reply_text('Такой записки не существет. Проверьте правильность написания названия')
             return 2
+
         else:
             await update.message.reply_photo(note.photo_id)
 
@@ -157,17 +162,18 @@ async def respond(update, context):
             "format": "json",
             "geocode": update.message.text
         })
+
         try:
             toponym = response["response"]["GeoObjectCollection"][
                 "featureMember"][0]["GeoObject"]
             ll, spn = get_ll_spn(toponym)
-
             static_api_request = f"http://static-maps.yandex.ru/1.x/?ll={ll}&spn={spn}&l=map"
             await context.bot.send_photo(
                 update.message.chat_id,
                 static_api_request,
                 caption=f"Карта по вашему запросу: {update.message.text}"
             )
+
         except IndexError:
             await update.message.reply_text('Место не найдено')
 
@@ -175,68 +181,25 @@ async def respond(update, context):
     return 1
 
 
-'''
-async def geocoder(update, context):
-    geocoder_uri = "http://geocode-maps.yandex.ru/1.x/"
-    response = await get_response(geocoder_uri, params={
-        "apikey": "40d1649f-0493-4b70-98ba-98533de7710b",
-        "format": "json",
-        "geocode": update.message.text
-    })
-
-    toponym = response["response"]["GeoObjectCollection"][
-        "featureMember"][0]["GeoObject"]
-    ll, spn = get_ll_spn(toponym)
-
-    static_api_request = f"http://static-maps.yandex.ru/1.x/?ll={ll}&spn={spn}&l=map"
-    await context.bot.send_photo(
-        update.message.chat_id,
-        static_api_request,
-        caption=f"Нашёл: {update.message.text}"
-    )
-    return ConversationHandler.END
-
-
-def get_ll_spn(toponym):
-    if not toponym:
-        return (None, None)
-
-    toponym_coordinates = toponym["Point"]["pos"]
-    toponym_longitude, toponym_lattitude = toponym_coordinates.split(" ")
-    ll = ",".join([toponym_longitude, toponym_lattitude])
-
-    envelope = toponym["boundedBy"]["Envelope"]
-
-    l, b = envelope["lowerCorner"].split(" ")
-    r, t = envelope["upperCorner"].split(" ")
-
-    dx = abs(float(l) - float(r)) / 2.0
-    dy = abs(float(t) - float(b)) / 2.0
-
-    span = f"{dx},{dy}"
-
-    return ll, span
-
-
-
-
-
-'''
-
-
 def main():
+
     application = Application.builder().token(BOT_TOKEN).build()
     application.add_handler(CommandHandler("stop", stop))
     application.add_handler(CommandHandler("help", help))
+
+    condition = (filters.TEXT | filters.PHOTO) & ~filters.COMMAND
+    handler_1 = MessageHandler(condition, get_name)
+    handler_2 = MessageHandler(condition, respond)
     conv_handler = ConversationHandler(
         entry_points=[CommandHandler('start', start)],
         states={
-            1: [MessageHandler((filters.TEXT | filters.PHOTO) & ~filters.COMMAND, get_name)],
-            2: [MessageHandler((filters.TEXT | filters.PHOTO) & ~filters.COMMAND, respond)]
+            1: [handler_1],
+            2: [handler_2]
         },
         fallbacks=[CommandHandler('stop', stop)]
     )
     application.add_handler(conv_handler)
+
     application.run_polling()
 
 
