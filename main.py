60eaedde2f0e54ea5aeb310e0447daa8c8df720f
GelberEkaterina <ek.gelber@gmail.com>
2024-04-28 23:40:26 +0700

    Everything works

--- main.py
+++ main.py
@@ -1,43 +1,37 @@
 import logging
-import asyncio
+
 from telegram import ReplyKeyboardMarkup
 from telegram.ext import Application, MessageHandler, filters, ConversationHandler, CommandHandler
-from aiogram import Bot, Dispatcher, types
-from aiogram.filters.command import Command
-from aiogram import F
-from aiogram.types import Message
-import aiohttp
-from sqlalchemy import Column, Integer, String
+
+from aiogram import Bot, Dispatcher
+
+import sqlalchemy
+from sqlalchemy import create_engine, Column, Integer, String, select
+from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import create_engine
-from sqlalchemy.orm import scoped_session, sessionmaker
-import os
 
 BOT_TOKEN = '7116319751:AAGoO4z5EKLLt8cML5seEGlhBFNF7_5K6ow'
+bot = Bot(BOT_TOKEN)
+dp = Dispatcher()
+
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.DEBUG
 )
-bot = Bot(BOT_TOKEN)
-dp = Dispatcher()
 logger = logging.getLogger(__name__)
 
 Base = declarative_base()
 
 
-class MediaIds(Base):
-    __tablename__ = 'Media ids'
+class Photo(Base):
+    __tablename__ = 'photos'
     id = Column(Integer, primary_key=True)
-    file_id = Column(String(255))
-    filename = Column(String(255))
+    photo_id = Column(String)
+    caption = Column(Integer)
 
 
-engine = create_engine(f'sqlite:///data.db')
-
-if not os.path.isfile(f'./data.db'):
-    Base.metadata.create_all(engine)
-
-session_factory = sessionmaker(bind=engine)
-Session = scoped_session(session_factory)
+engine = create_engine('sqlite:///data.db')
+Session = sessionmaker(bind=engine)
+Base.metadata.create_all(engine)
 
 
 async def help(update, context):
@@ -53,21 +47,52 @@ async def stop(update, context):
 
 
 async def start(update, context):
-    reply_keyboard = [['Создать заметку'],
-                      ['Открыть заметку', 'Создать напоминание']]
-    markup = ReplyKeyboardMarkup(reply_keyboard)
+    reply_keyboard = [['Создать заметку', 'Список заметок'],
+                      ['Открыть заметку', 'Очистить заметки']]
+    markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True)
     await update.message.reply_text("Что Вы хотите сделать?", reply_markup=markup)
     return 1
 
 
 async def get_name(update, context):
+    session = Session()
     context.user_data['state'] = update.message.text
+
     if context.user_data['state'] == 'Создать заметку':
         await update.message.reply_text("Отправьте фотографию для заметки с её названием в подписи")
+
     elif context.user_data['state'] == 'Открыть заметку':
-        await update.message.reply_text('Введите название файла')
+        query = select(Photo.caption)
+        list_of_notes = session.execute(query).fetchall()
+        ans = list(filter(lambda y: y is not None, map(lambda x: x[0], list_of_notes)))
+        if len(ans) > 0:
+            await update.message.reply_text('Введите название заметки')
+        else:
+            await update.message.reply_text('Заметок нет')
+            return 1
+
+    elif context.user_data['state'] == 'Список заметок':
+        query = select(Photo.caption)
+        list_of_notes = session.execute(query).fetchall()
+        ans = list(filter(lambda y: y is not None, map(lambda x: x[0], list_of_notes)))
+        if len(ans) > 0:
+            await update.message.reply_text('\n'.join(ans))
+        else:
+            await update.message.reply_text('Заметок нет')
+        session.close()
+        return 1
+
+    elif context.user_data['state'] == 'Очистить заметки':
+        session = Session()
+        session.query(Photo).delete()
+        session.commit()
+        await update.message.reply_text('Заметки успешно удалены')
+        session.close()
+        return 1
+
     elif context.user_data['state'] == 'Создать напоминание':
         await update.message.reply_text('Введите текст напоминания')
+
     else:
         await update.message.reply_text('Выберите один из вариантов')
         return 1
@@ -75,33 +100,31 @@ async def get_name(update, context):
 
 
 async def respond(update, context):
+    session = Session()
+
     if context.user_data['state'] == 'Создать заметку':
-        await update.message.reply_photo(update.message.photo[-1].file_id, caption=update.message.caption)
-    if context.user_data['state'] == 'Открыть заметку':
-        await update.message.reply_text()
-    if context.user_data['state'] == 'Создать напоминание':
+        new_note = Photo(photo_id=update.message.photo[-1].file_id, caption=update.message.caption)
+        if update.message.caption is None:
+            await update.message.reply_text('Для создания заметки необходима подпись. Отправьте изображение ещё раз')
+            return 2
+        else:
+            session.add(new_note)
+            session.commit()
+            await update.message.reply_text('Заметка "{}" успешно сохранена!'.format(update.message.caption))
+
+    elif context.user_data['state'] == 'Открыть заметку':
+        note = session.query(Photo).filter_by(caption=update.message.text).order_by(-Photo.id).first()
+        if note is None:
+            await update.message.reply_text('Такой записки не существет. Проверьте правильность написания названия')
+            return 2
+        else:
+            await update.message.reply_photo(note.photo_id)
+
+    elif context.user_data['state'] == 'Создать напоминание':
         await update.message.reply_text('Ф')
-    return ConversationHandler.END
 
-
-async def view_notes(message: types.Message, context):
-    await message.reply("Какую заметку вы бы хотели посмотреть?")
-
-
-async def get_response(url, params):
-    logger.info(f"getting {url}")
-    async with aiohttp.ClientSession() as session:
-        async with session.get(url, params=params) as resp:
-            return await resp.json()
-
-
-async def reminder(message: types.Message):
-    await message.reply("Введите текст напоминания")
-
-
-@dp.message(F.photo)
-async def echo_gif(message: Message):
-    await message.reply_photo(message.photo[-1].file_id)
+    session.close()
+    return 1
 
 
 def main():
